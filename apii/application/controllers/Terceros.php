980f0fc8d863acceff9f8c050ce947945a0fe116
Miguelort095 <31443985+Miguelort095@users.noreply.github.com>
2019-08-31 22:32:37 -0500

    Actualizacion

---
+++ apii/application/controllers/Terceros.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Terceros extends CI_Controller {
+
+	public function Terceros()
+	{
+		$accion = $this->uri->segment(3,'0');
+		switch ($accion) {
+			case '0':
+			case 'gestionar':
+				if ($this->uri->segment(4) == "guardar") {
+
+				$query = $this->Registro->RegistrarTercero();
+				json_encode($query);
+				}
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+	}
+
+}
+
+/* End of file Terceros.php */
+/* Location: ./application/controllers/Terceros.php */
